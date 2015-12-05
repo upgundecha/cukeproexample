aeb9653051449be2d11c2b4da58705ba54dce0e8
Unmesh Gundecha <unmeshtm@gmail.com>
2015-12-05 17:28:25 +0530

    Updated POM and step defs

--- src/test/java/com/testomatic/cuke/StepDefs.java
+++ src/test/java/com/testomatic/cuke/StepDefs.java
@@ -9,14 +9,20 @@ import cucumber.api.java.en.When;
  */
 public class StepDefs {
 
-    @When("^user request for list of Posts published on Blog$")
-    public void user_request_for_list_of_Posts_published_on_Blog() throws Throwable {
+    @When("^user request for list of posts published on blog$")
+    public void user_request_for_list_of_posts_published_on_blog() throws Throwable {
         // Write code here that turns the phrase above into concrete actions
         throw new PendingException();
     }
 
     @Then("^status code should be (\\d+)$")
     public void status_code_should_be(int arg1) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^response should have a list of posts$")
+    public void response_should_have_a_list_of_posts() throws Throwable {
         // Write code here that turns the phrase above into concrete actions
         throw new PendingException();
     }
